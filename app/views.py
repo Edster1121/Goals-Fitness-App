28e3ee2c966a8b46d89b78b1c130dd11c57bd077
Eddie Yoshie <eddieyoshie@gmail.com>
2025-08-27 20:01:04 -0700

    Added basic retrieval of data from db

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from .models import Workout
 
-# Create your views here.
-def index(request):
-    return render(request, 'app/index.html')
+def workout_list(request):
+    workouts = Workout.objects
+    return render(request, "app/index.html", {"workouts": workouts})
